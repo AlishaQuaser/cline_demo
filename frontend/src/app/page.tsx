1d4dab50359ecd6a888a020b10aed184b95cd1f2
Alisha Quaser <alisha.infomover@gmail.com>
2025-06-26 00:18:29 +0530

    add files

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatInterface from '@/components/ChatInterface'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-4">
-      <ChatInterface />
-    </main>
-  )
-}
